91ed0475a2463f0a5e4c9b8f18ff10c2043fad44
bpadros <bpadros@gmail.com>
2022-06-01 14:46:08 -0300

    se agrego la logica para mostrar la lista de productos

---
+++ src/components/productos/ItemProducto.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ListGroup, Button } from 'react-bootstrap';
+
+const ItemProducto = (props) => {
+    return (
+        <ListGroup.Item className="d-flex justify-content-between">
+        <p>
+          {props.producto.nombreProducto}
+          <span className='fw-bolder'>-Precio:$ {props.producto.precioProducto}</span>
+        </p>
+        <div>  
+        <Button variant="warning">Editar</Button>
+          <Button variant="danger">Borrar</Button>
+        </div>
+      </ListGroup.Item>
+    );
+};
+
+export default ItemProducto;
---
+++ src/components/productos/ListaProductos.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container, ListGroup, Button } from "react-bootstrap";
+import ItemProducto from "./ItemProducto";
+
+const ListaProductos = (props) => {
+  return (
+    <Container className="my-5">
+      <h1 className="text-center mb-5">Lista de productos</h1>
+      <hr />
+      <article className="d-flex align-items-center mb-4 justify-content-end">
+        <p className="my-0 me-4 fw-bold">Agregar nuevos productos: </p>
+        <Link to="/productos/nuevo" className="btn btn-primary"> Agregar</Link>
+      </article>
+      <ListGroup>
+        {
+          props.productos.map((producto)=><ItemProducto key={producto.id} producto={producto}></ItemProducto>)
+        }  
+      </ListGroup>
+    </Container>
+  );
+};
+
+export default ListaProductos;
